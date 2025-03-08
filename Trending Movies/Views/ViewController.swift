81bf1a448a25623ac1538ade709cbfdb704e6433
ArilsonFJS <arilsonsilva61@gmail.com>
2025-03-08 18:50:38 -0300

    Iniciando projeto

---
+++ Trending Movies/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trending Movies
+//
+//  Created by Arilson Silva on 08/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
