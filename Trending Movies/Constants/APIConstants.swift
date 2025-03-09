6564c391dd32c8f1877ddad5f45d4e1d8e356573
ArilsonFJS <arilsonsilva61@gmail.com>
2025-03-09 00:34:49 -0300

    Criando constante da api e realizando tratamento da url

---
+++ Trending Movies/Constants/APIConstants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct APIConstants {
+    
+    static let apiKey = "SUA API_KEY"
+    static let baseURL = "https://api.themoviedb.org/3"
+}
---
+++ Trending Movies/Services/MovieService.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+class MovieService {
+    
+    func fetchTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        
+        let urlString = "\(APIConstants.baseURL)/movie/popular?api_key=\(APIConstants.apiKey)&language=pt-BR&page=1"
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NSError(domain: "URL Inválida", code: 0, userInfo: nil)))
+            return
+        }
+    }
+}
