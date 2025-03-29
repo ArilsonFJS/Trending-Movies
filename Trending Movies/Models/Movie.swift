0862bfd9fb42b6c1454a93dba2f51df365d7294a
ArilsonFJS <arilsonsilva61@gmail.com>
2025-03-29 17:19:30 -0300

    ajustando tipo do voteAverage e colocando voteAverage como opcional

--- Trending Movies/Models/Movie.swift
+++ Trending Movies/Models/Movie.swift
@@ -1,12 +1,12 @@
 struct Movie: Codable {
     let id: Int
     let title: String
-    let overview: String
-    let voteAverage: Int?
+    let overview: String?
+    let voteAverage: Double?
     let posterPath: String?
     let releaseDate: String?
-    
-    enum CondingKeys: String, CodingKey {
+
+    enum CodingKeys: String, CodingKey {
         case id, title, overview
         case voteAverage = "vote_average"
         case posterPath = "poster_path"
